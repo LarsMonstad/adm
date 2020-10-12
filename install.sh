c55e7699e423f41fa60577311e88928dd6894bf0
lrs11 <lars.monstad@gmail.com>
2020-10-12 02:04:58 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-
+#https://github.com/lrs11/adm
 #script for installing and implementing the deep learning model and midifiles for bachprop
 apt update
 apt install python3-pip -y
@@ -19,9 +19,10 @@ sleep 5
 #getting files_____________________________
 git clone https://github.com/FlorianColombo/BachProp
 cd BachProp-master/data/
+mkdir -p B7/midi 
 wget "http://larsvegas1.com.datasenter.no/Batch7.rar" -O Batch7.rar
-unrar x Batch7.rar
-cd ../src/
+unrar e Batch7.rar B7/midi/
+cd ../../src/
 #need to install libcudnn otherwise the script wont run... ______________________________________
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
@@ -30,6 +31,10 @@ dpkg -i libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 #starting the training___________ 
+apt install sed -y
+sed -i 'os.environ["CUDA_VISIBLE_DEVICES"] = "-1"' BachProp.py
+head BachProp.py
+sleep 10
 python3 BachProp.py B7 train
 
 #uploadning files to ftp ____________________________________________
@@ -37,4 +42,6 @@ cd ../save
 apt install rar -y
 rar B7.rar B7 
 apt install ftp-upload -y 
-ftp-upload -h linweb07.sbv.webhuset.no -u xxx --password xxxx -d batch7 B7.rar
+ftp-upload -h linweb07.sbv.webhuset.no -u u1234 --password p1234 -d batch7 B7.rar
+#to run the script write: git clone https://github.com/lrs11/adm
+
