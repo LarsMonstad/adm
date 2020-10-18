730455bf16b4de47a039ce82f2979c5e4daf6980
lrs11 <lars.monstad@gmail.com>
2020-10-19 01:25:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cd BachProp/data/
 mkdir -p B7/midi 
 wget "http://larsvegas1.com.datasenter.no/Batch/Batch7.rar" -O Batch7.rar
 unrar e Batch7.rar B7/midi/
-cd ../../src/
+cd ../src/
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 dpkg -i libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
@@ -28,13 +28,13 @@ dpkg -i libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 apt install -y sed
-sed -i 'os.environ["CUDA_VISIBLE_DEVICES"] = "-1"' BachProp.py
+sed -i '6 a os.environ["CUDA_VISIBLE_DEVICES"] = "-1"' BachProp.py
 head BachProp.py
 sleep 10
 python3 BachProp.py B7 train
 cd ../save
 apt install -y rar
-rar B7.rar B7 
+rar a B7.rar B7 
 apt install -y ftp-upload
-ftp-upload -h linweb07.sbv.webhuset.no -u u1234 --password p1234 -d batch7 B7.rar
+#ftp-upload -h linweb07.sbv.webhuset.no -u u1234 --password p1234 -d batch7 B7.rar
 
