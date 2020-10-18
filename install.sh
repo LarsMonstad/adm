ced3234d4559751e41c28715d62599fbb561caf9
lrs11 <lars.monstad@gmail.com>
2020-10-19 00:54:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-#https://github.com/lrs11/adm
-#script for installing and implementing the deep learning model and midifiles for bachprop
 apt update
 apt install -y python3-pip 
 alias python==python3;alias pip==pip3
 python3 -m pip install --upgrade pip
 pip3 install tensorflow==2.2.0
-pip install mido  
+pip install mido
 pip install keras 
 apt install -y git  
 pip install git+https://github.com/louisabraham/python3-midi #the only midi package that works with bachprop
@@ -16,32 +14,26 @@ pip install tqdm
 apt install -y wget
 pip list
 sleep 5
-#getting files_____________________________
 git clone https://github.com/FlorianColombo/BachProp
-cd BachProp-master/data/
+cd BachProp/data/
 mkdir -p B7/midi 
 wget "http://larsvegas1.com.datasenter.no/Batch/Batch7.rar" -O Batch7.rar
 unrar e Batch7.rar B7/midi/
 cd ../../src/
-#need to install libcudnn otherwise the script wont run... ______________________________________
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 dpkg -i libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 dpkg -i libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
-#starting the training___________ 
 apt install -y sed
 sed -i 'os.environ["CUDA_VISIBLE_DEVICES"] = "-1"' BachProp.py
 head BachProp.py
 sleep 10
 python3 BachProp.py B7 train
-
-#uploadning files to ftp ____________________________________________
 cd ../save
 apt install -y rar
 rar B7.rar B7 
 apt install -y ftp-upload
 ftp-upload -h linweb07.sbv.webhuset.no -u u1234 --password p1234 -d batch7 B7.rar
-#to run the script write: git clone https://github.com/lrs11/adm
 
