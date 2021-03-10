56d44fbf317134a8b0e4e293dbf06de5dd3f51ef
lrs11 <lars.monstad@gmail.com>
2021-03-10 23:31:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
+#set number of epochs, temperature, url for midifile,ftp settings,
+#example - ./start.sh -e 600 -t "0.4" -f "http://ftp.com/batch.rar" -l "ftp://myftpserevr.com" -u "ftpusername" -p "ftppassword" -d "/ftpfolder" -b "batch number"
+while getopts e:t:f:l:u:p:d:b: flag
+do
+    case "${flag}" in
+        e) epochnumber=${OPTARG};;
+        t) tempnumber=${OPTARG};;
+        f) file=${OPTARG};;
+        l) ftpurl=${OPTARG};;
+        u) ftpu=${OPTARG};;
+        p) ftppw=${OPTARG};;
+        d) ftpfolder=${OPTARG};;
+        b) batchnr=${OPTARG};;		
+    esac
+done
+#start install of everything
 apt update
-apt install -y python3-pip 
+for x in python3-pip git unrar unzip wget rar ncftp sed
+do
+	apt install -y $x
+done 
 alias python==python3;alias pip==pip3
 python3 -m pip install --upgrade pip
 pip3 install tensorflow==2.2.0
-pip install mido
-pip install keras
-apt install -y git
-pip install git+https://github.com/louisabraham/python3-midi
-apt install -y unrar 
-apt install -y unzip 
-pip install h5py
-pip install tqdm
-apt install -y wget
+for y in mido keras git+https://github.com/louisabraham/python3-midi h5py tqdm
+do
+	pip install $y
+done
 pip list
 sleep 5
 git clone https://github.com/FlorianColombo/BachProp
 cd BachProp/data/
 mkdir -p B7/midi 
-wget "http://larsvegas1.com.datasenter.no/Batch/Batch7.rar" -O Batch7.rar
+wget $file -O Batch7.rar
 unrar e Batch7.rar B7/midi/
 cd ../src/
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
@@ -27,14 +41,21 @@ dpkg -i libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 dpkg -i libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7_7.6.4.38-1+cuda10.1_amd64.deb
 rm libcudnn7-dev_7.6.4.38-1+cuda10.1_amd64.deb
-apt install -y sed
+#Remove gpu processing temporarily
 sed -i '6 a os.environ["CUDA_VISIBLE_DEVICES"] = "-1"' BachProp.py
 head BachProp.py
+#Setting epoch number
+var1="    def trainModel(self, epochs=$epochnumber, validation_split=0.1):"
+sed -i "407s|^.*$|$var1|" BachProp.py 
+#Setting temperature
+var2="    def generate(self, note_len=1000, until_all_ended=True, temperature=$tempnumber, seed=None):"
+sed -i "641s|^.*$|$var2|" BachProp.py 
 sleep 10
+#Training
 python3 BachProp.py B7 train
 cd ../save
-apt install -y rar
-rar a B7.rar B7 
-apt install -y ftp-upload
-#ftp-upload -h linweb07.sbv.webhuset.no -u u1234 --password p1234 -d batch7 B7.rar
-
+#create a rar file
+rar a ${batchnr}.rar B7
+# upload the file into ftp 
+ncftpput -u $ftpu -p $ftppw $ftpurl $ftpfolder ${batchnr}.rar
+sleep 20
